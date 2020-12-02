e9eee341559d1ca197d8f9a2e1c34f7402b93496
Yuki Tosa <yuki.tosa.simplemind@gmail.com>
2020-12-02 18:31:59 +0900

    + ROE 50%

--- 3_Make_index.py
+++ 3_Make_index.py
@@ -9,5 +9,13 @@ tmp_result = pd.read_excel(in_workbook_pass)
 
 result = tmp_result[['Breakdown', 'Total Revenue', 'Net Income Common Stockholders', 'Total Assets', 'Common Stock Equity', 'Ordinary Shares Number']]
 
+result['Total Revenue TTM'] = result['Total Revenue'] + result['Total Revenue'].shift(1) + result['Total Revenue'].shift(2) + result['Total Revenue'].shift(3)
+result['Net Income Common Stockholders TTM'] = result['Net Income Common Stockholders'] + result['Net Income Common Stockholders'].shift(1) + result['Net Income Common Stockholders'].shift(2) + result['Net Income Common Stockholders'].shift(3)
+result['Total Assets ATTM'] = (result['Total Assets'] + result['Total Assets'].shift(1) + result['Total Assets'].shift(2) + result['Total Assets'].shift(3)) / 4
+result['Common Stock Equity ATTM'] = (result['Common Stock Equity'] + result['Common Stock Equity'].shift(1) + result['Common Stock Equity'].shift(2) + result['Common Stock Equity'].shift(3)) / 4
+
+result_copy = result.copy()
+result_fillna = result_copy.fillna('-')
+
 result_pass = '../Result/{0}_result.xlsx'.format(name)
-result.to_excel(result_pass, index=False)
+result_fillna.to_excel(result_pass, index=False)
