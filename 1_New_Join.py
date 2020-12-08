11b5593fb4592a33c703e604a1e6dcb7b69a34e6
Yuki Tosa <yuki.tosa.simplemind@gmail.com>
2020-12-08 14:21:39 +0900

    Sheet.valuesをpd.DataFrameへと変換

--- 1_New_Join.py
+++ 1_New_Join.py
@@ -2,52 +2,52 @@ name = input('ティッカーシンボルを入力: ')
 
 
 import openpyxl
-
-# バランスシート
-out_bsf_workbook = openpyxl.Workbook()
-out_bsf_sheet = out_bsf_workbook.active
-
-in_bsf_workbook = openpyxl.load_workbook('../2_Format/BalanceSheet_Format.xlsx', data_only=True)
-in_bsf_sheet = in_bsf_workbook['BS']
-
-for i in range(1, 7):
-    for j in range(1, 100):
-        copy = in_bsf_sheet.cell(row = i, column =j+2).value
-        out_bsf_sheet.cell(row = i, column =j , value = copy)
-
-out_bsf_filename = 'bsf_new_result.xlsx'
-out_bsf_workbook.save(out_bsf_filename)
-
-# インカムステートメント
-out_is_workbook = openpyxl.Workbook()
-out_is_sheet = out_is_workbook.active
-
-in_is_workbook = openpyxl.load_workbook('../2_Format/IncomeStatement_Format.xlsx', data_only=True)
-in_is_sheet = in_is_workbook['IS']
-
-for i in range(1, 8):
-    for j in range(1, 100):
-        copy = in_is_sheet.cell(row = i, column =j+2).value
-        out_is_sheet.cell(row = i, column =j , value = copy)
-
-out_is_filename = 'is_new_result.xlsx'
-out_is_workbook.save(out_is_filename)
-
-
 import pandas as pd
 
-# 両者をマージ
-bsf_result = pd.read_excel(out_bsf_filename)
-is_result = pd.read_excel(out_is_filename)
 
-name_result = pd.merge(bsf_result, is_result, on='Breakdown')
+# バランスシート
+
+in_bsf_workbook = openpyxl.load_workbook('../2_Format/BalanceSheet_Format.xlsx', read_only=True, data_only=True)
+in_bsf_sheet = in_bsf_workbook[in_bsf_workbook.sheetnames[0]]
+
+# Sheet.valuesをpd.DataFrameへと変換
+bsf_data = in_bsf_sheet.values
+bsf_data_list = list(bsf_data)
+bsf_df = pd.DataFrame(bsf_data_list[1:], columns=bsf_data_list[0])
+
+# 6行目以降を削除
+bsf_df_tmp_1 = bsf_df.iloc[range(5), :]
+
+# 同一名のカラムのうち、一番目を削除し、二番目を残す
+bsf_df_tmp_2 = bsf_df_tmp_1.loc[:,~bsf_df_tmp_1.columns.duplicated(keep = "last")]
+
+# 一列目を削除
+bsf_df_result = bsf_df_tmp_2.drop(bsf_df_tmp_2.columns[0], axis=1)
+
+
+# インカムステートメント
+
+in_isf_workbook = openpyxl.load_workbook('../2_Format/IncomeStatement_Format.xlsx', read_only=True, data_only=True)
+in_isf_sheet = in_isf_workbook[in_isf_workbook.sheetnames[0]]
+
+# Sheet.valuesをpd.DataFrameへと変換
+isf_data = in_isf_sheet.values
+isf_data_list = list(isf_data)
+isf_df = pd.DataFrame(isf_data_list[1:], columns=isf_data_list[0])
+
+# 7行目以降を削除
+isf_df_tmp_1 = isf_df.iloc[range(6), :]
+
+# 同一名のカラムのうち、一番目を削除し、二番目を残す
+isf_df_tmp_2 = isf_df_tmp_1.loc[:,~isf_df_tmp_1.columns.duplicated(keep = "last")]
+
+# 一行目と一列目を削除
+isf_df_result = isf_df_tmp_2.drop(index=isf_df_tmp_2.index[0], columns=isf_df_tmp_2.columns[0])
+
+
+# 両者をマージ
+
+name_result = pd.merge(bsf_df_result, isf_df_result, on='Breakdown')
 
 name_result_pass = '../1_Storage/{0}_new_join.xlsx'.format(name)
 name_result.to_excel(name_result_pass, index=False)
-
-
-import os
-
-# 不要なファイルを削除
-os.remove(out_bsf_filename)
-os.remove(out_is_filename)
